ce89880b24824b8e50d1fef4bfc7886e9401ae32
Xeni-ya <budyuginaxeniya@gmail.com>
2024-11-17 06:21:41 +0300

    fix: I adjusted the callback button

--- assets/script/_header.js
+++ assets/script/_header.js
@@ -6,3 +6,18 @@ btnMenu.addEventListener('click', function() {
 
   btnMenu.setAttribute('aria-expanded', openMenu);
 });
+
+//смена картинки при наведении
+const phoneButtons = document.querySelectorAll('.phone__button-callback');
+
+phoneButtons.forEach(button => {
+  button.addEventListener('mouseover', () => {
+    const img = button.querySelector('.phone__icon');
+    img.src = '../icon/telephone_icon_white.svg'; 
+  });
+
+  button.addEventListener('mouseout', () => {
+    const img = button.querySelector('.phone__icon');
+    img.src = '../icon/telephone_icon.svg'; 
+  });
+});
